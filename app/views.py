f00a0b2898bda0e8d53c1fe7e320fb9075a9f56f
divyavani42000 <divyavani42000@gmail.com>
2023-11-21 12:23:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def divya(request):
+    return render(request,'divya.html')
+def bhagya(request):
+    return render(request,'bhagya.html')
